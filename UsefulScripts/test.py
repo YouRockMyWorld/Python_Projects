d6449e174d66531ed9757328be1c589083988cf5
YouRockMyWorld <sc_yourockmyworld@163.com>
2019-03-01 11:44:31 +0800

    Update test.py

--- UsefulScripts/test.py
+++ UsefulScripts/test.py
@@ -3,7 +3,7 @@ from Date import *
 import re, os
 
 if __name__ == '__main__':
-    files = get_all_files(r'C:\Users\Administrator\Desktop\集成村站转换\xls', '*.xls')
+    files = get_all_files(r'C:\Users\Administrator\Desktop\集站转换\xls', '*.xls')
     files.sort(key=lambda x: int(re.findall(r'\d+', x)[0]))
     date = get_date_list(start_date='_2018-06-23', end_date='_2018-12-27', format='_%Y-%m-%d')
     print(date)
